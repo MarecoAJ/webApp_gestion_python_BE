dc4a7031a4d8516123c9d7d584d5f44cbc956202
MarecoAJ <alejo_mareco@hotmail.com>
2023-10-26 11:04:14 -0300

    manejo de errores

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
+from middlewares.error_handler import ErrorHandler
 
+# instancia de aplicacion
 app = FastAPI()
+
+# datos para documentacion Swagger
+app.title = "Sistema de gestion"
+app.version = "0.0.1"
+
+#manejo de errores
+app.add_middleware(ErrorHandler)
